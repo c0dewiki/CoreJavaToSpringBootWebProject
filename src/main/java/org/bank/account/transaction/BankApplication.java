e3609d6b3c2a65d6acf95007a3b817c68764e6c6
0CodeHunter0 <tejagundabattina@gmail.com>
2023-11-29 08:01:03 +0530

    Commiting of code Spring Core

--- src/main/java/org/bank/account/transaction/BankApplication.java
+++ src/main/java/org/bank/account/transaction/BankApplication.java
@@ -3,22 +3,24 @@ package org.bank.account.transaction;
 
 
 import org.bank.account.transaction.api.AccountTransactionAPI;
-import org.bank.account.transaction.service.EmailNotificationClient;
-import org.bank.account.transaction.service.SmsNotificationClient;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-
+@Configuration
+@ComponentScan
 public class BankApplication 
 {
-    public static void main( String[] args ) {
+    @SuppressWarnings("resource")
+	public static void main( String[] args ) {
     	
     	
     
-        AccountTransactionAPI accountTransactionAPI = 
-        		new AccountTransactionAPI();
-        EmailNotificationClient emailNotificationClient = new EmailNotificationClient();
-        SmsNotificationClient smsNotificationClient = new SmsNotificationClient();
-        accountTransactionAPI.setEmailNotificationClient(emailNotificationClient);
-        accountTransactionAPI.setSmsNotificationClient(smsNotificationClient);
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(BankApplication.class);
+        
+         AccountTransactionAPI accountTransactionAPI = applicationContext.getBean(AccountTransactionAPI.class);
+        
         
         System.out.println("Account debit Transaction details");
         accountTransactionAPI.sendDebitTransaction();
--- src/main/java/org/bank/account/transaction/api/AccountTransactionAPI.java
+++ src/main/java/org/bank/account/transaction/api/AccountTransactionAPI.java
@@ -1,11 +1,15 @@
 package org.bank.account.transaction.api;
 
 import org.bank.account.transaction.service.NotificationClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-
+@Component
 public class AccountTransactionAPI {
 	
+	@Autowired
 	private NotificationClient emailNotificationClient;
+	@Autowired
 	private NotificationClient smsNotificationClient;
 	
 	public void sendDebitTransaction() {
@@ -18,12 +22,6 @@ public class AccountTransactionAPI {
 
 	}
 
-	public void setEmailNotificationClient(NotificationClient emailNotificationClient) {
-		this.emailNotificationClient = emailNotificationClient;
-	}
-
-	public void setSmsNotificationClient(NotificationClient smsNotificationClient) {
-		this.smsNotificationClient = smsNotificationClient;
-	}
+	
 	
 }
